ae73995786a1f5c4db434c83ada0c491d33596ca
Muden <mudennn@yahoo.com>
2022-08-03 15:47:20 +0800

    first commit

---
+++ smart-contract/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
+
+//Exchange Contract Address: 0x229632e5aB751EF8F7540378d6440436Bf8C1F05
